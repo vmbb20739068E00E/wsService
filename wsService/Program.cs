65bfad0306301ccff190b38dc0b36edad3c03e11
Vasiliy Moiseev <vmoiseev@baltbet.ru>
2018-12-28 13:50:01 +0300

    [svc]

---
+++ wsService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace wsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceHost =>
+            {
+                serviceHost.Service<Service>(config =>
+                    {
+                        config.ConstructUsing(() => new Service());
+                        config.WhenStarted(svc => svc.Start());
+                        config.WhenStopped(svc => svc.Stop());
+                    }
+                );
+
+                serviceHost.StartAutomatically();
+                serviceHost.SetDescription("Test Windows Service");
+                serviceHost.SetDisplayName("Test Windows Service");
+                serviceHost.SetServiceName("testWsService");
+            });
+        }
+    }
+}
---
+++ wsService/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wsService
+{
+    public class Service
+    {
+        public bool Start()
+        {
+            return true;
+        }
+
+        public bool Stop()
+        {
+            return true;
+        }
+    }
+}
